feb9dc7cdc712db0fb889f8301d0629c60e926c2
travisholt92 <travisholt92@gmail.com>
2020-09-28 19:05:02 -0400

    fix derps

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -41,11 +41,11 @@ except ImportError:
 
 DEBUG = False
 
-custom_local_manifest = ".repo/local_manifests/local_corvus.xml"
+custom_local_manifest = ".repo/local_manifests/local_kang.xml"
 custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', '10')
-custom_dependencies = "corvus.dependencies"
+custom_dependencies = "kang.dependencies"
 org_manifest = "device"  # leave empty if org is provided in manifest
-org_display = "CorvusROM-Devices"  # needed for displaying
+org_display = "KangOS-11"  # needed for displaying
 
 github_auth = None
 
